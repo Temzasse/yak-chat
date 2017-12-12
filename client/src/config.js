5ce79183b0fa3afbc43afd2b4f874ac468b876c4
Teemu Taskula <teemu.taskula@aalto.fi>
2017-12-12 13:37:44 +0200

    Fix demo setup

--- client/src/config.js
+++ client/src/config.js
@@ -1,15 +1,15 @@
 const IS_PROD = process.env.NODE_ENV === 'production';
-// const IS_DEMO = process.env.DEMO === 'true';
+const IS_DEMO = process.env.REACT_APP_DEMO === 'true';
 
-// let API_URL = IS_PROD
-//   ? 'http://0.0.0.0:3332'
-//   : `http://${window.location.host}/`;
+let API_URL = IS_PROD
+  ? 'http://0.0.0.0:3332'
+  : `http://${window.location.host}/`;
 
-// if (IS_DEMO) API_URL = 'https://0.0.0.0:3332';
+if (IS_DEMO) API_URL = 'https://0a4910cb.ngrok.io';
 
 const config = {
   IS_PROD,
-  API_URL: 'https://d79fdd09.ngrok.io',
+  API_URL,
 };
 
 export default config;
